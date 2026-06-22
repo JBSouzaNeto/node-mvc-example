9c9ab4abdebe6217965df55ebb1c4ef6812390c8
João Batista de Souza Neto <jbsneto23@gmail.com>
2026-06-22 17:06:50 -0300

    ADD files and adjusting config

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ var app = express();
 
 var autoresRouter = require('./routes/autores');
 
-
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -17,20 +16,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/autores', autoresRouter); 
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(3000, () => {
   console.log('Servidor iniciado na porta 3000...')
